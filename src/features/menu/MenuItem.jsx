c8e9eb8aebf276aa775cc6be16cc5e4dcf9b0952
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-07-09 17:31:50 +0400

    start project, Setting up File Structure, Implementing Routes

---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,22 @@
+import { formatCurrency } from "../../utils/helpers";
+import PropTypes from "prop-types";
+function MenuItem({ pizza }) {
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  return (
+    <li>
+      <img src={imageUrl} alt={name} />
+      <div>
+        <p>{name}</p>
+        <p>{ingredients.join(", ")}</p>
+        <div>
+          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        </div>
+      </div>
+    </li>
+  );
+}
+MenuItem.propTypes = {
+  pizza: PropTypes.string.isRequired,
+};
+export default MenuItem;
